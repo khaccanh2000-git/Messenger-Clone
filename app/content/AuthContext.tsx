ac8aff505bd608afb8392648d175ae947f84cc82
Khac Canh <khaccanh2000bn@gmail.com>
2024-01-05 13:17:28 +0700

    Sidebar, Navigation

---
+++ app/content/AuthContext.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import exp from "constants";
+import { SessionProvider } from "next-auth/react";
+
+interface AuthContextProps {
+  children: React.ReactNode;
+}
+
+export default function AuthContext({ children }: AuthContextProps) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
